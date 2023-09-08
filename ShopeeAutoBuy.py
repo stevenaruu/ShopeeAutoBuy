b69a0cb4433fa0c49d7406309bd8c6394f5affa6
Steven Forsythia <stvnhd164@gmail.com>
2023-09-08 11:54:29 +0700

    feat: add auto buy clicker

--- ShopeeAutoBuy.py
+++ ShopeeAutoBuy.py
@@ -11,21 +11,51 @@ load_dotenv()
 LOGIN_VALUE = os.getenv("LOGIN_VALUE")
 PASSWORD_VALUE = os.getenv("PASSWORD_VALUE")
 
-def setupShopee():
+def AutoBuy(driver):
+    # size button is optional, depend the market have an option or not
+    size_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="42"]')))
+    size_button.click()
+
+    submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="beli sekarang"]')))
+    submit_button.click()
+
+    checkout_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="checkout"]')))
+    checkout_button.click()
+
+    order_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Buat Pesanan"]')))
+    order_button.click()
+
+def SetupShopee(link_product):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
 
     driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
     driver.get("https://shopee.co.id/buyer/login")
 
-    login = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.NAME, "loginKey")))
+    login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "loginKey")))
     login.send_keys(LOGIN_VALUE)
 
-    password = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.NAME, "password")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "password")))
     password.send_keys(PASSWORD_VALUE)
 
-    login_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/div/div[2]/div/div/div/div[2]/form/div/div[2]/button')))
+    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/div/div[2]/div/div/div/div[2]/form/div/div[2]/button')))
     login_button.click()
 
-setupShopee()
+    success = WebDriverWait(driver, 10).until(EC.url_to_be("https://shopee.co.id/?is_from_login=true"))
+
+    if(success):
+        print("Login success")
+        driver.get(link_product)
+        
+        wait = WebDriverWait(driver, 10).until(EC.url_to_be(link_product))
+        
+        if(wait):
+            AutoBuy(driver)
+    else:
+        print("Cannot login")
+        print("Try re-login")
+        SetupShopee(link_product)
+
+link_product = input('Shopee Product Link: ')
+SetupShopee(link_product)
 
